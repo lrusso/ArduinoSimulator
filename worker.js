4c1ab53b1875ea5ed94fda05b652492dc47cc69a
Leonardo Russo <info@lrusso.com>
2020-09-02 22:45:58 -0300

    Add files via upload

---
+++ worker.js
@@ -0,0 +1,39 @@
+const filesToCache = [
+	"ArduinoSimulator.css",
+	"ArduinoSimulator.htm",
+	"ArduinoSimulator.js",
+	"ArduinoSimulator.json",
+	"ArduinoSimulator.png",
+	"ArduinoSimulatorEditor.js",
+	"ArduinoSimulatorFavIcon_16x16.png",
+	"ArduinoSimulatorFavIcon_192x192.png",
+	"ArduinoSimulatorFavIcon_512x512.png",
+	"ArduinoSimulatorInterpreter.js",
+	"ArduinoSimulatorInterpreter.min.js",
+	"ArduinoSimulatorSample.htm",
+	"ArduinoSimulatorShare.png"
+];
+
+const staticCacheName = "arduinosimulator-v1";
+
+self.addEventListener("install", event => {
+	event.waitUntil(
+		caches.open(staticCacheName)
+		.then(cache => {
+			return cache.addAll(filesToCache);
+		})
+	);
+});
+
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.match(event.request)
+		.then(response => {
+			if (response) {
+				return response;
+			}
+			return fetch(event.request)
+		}).catch(error => {
+		})
+	);
+});
